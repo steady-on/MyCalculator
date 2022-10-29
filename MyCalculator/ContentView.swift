77901de9d2dbf9d9b0779674055575400f296f04
whitebsm93 <whitebsm93@naver.com>
2022-10-30 05:16:32 +0900

    Add function inputNumber
    - 숫자 입력 함수 추가 및 적용
    - calculate 객체 불러오기 및 적용

--- MyCalculator/ContentView.swift
+++ MyCalculator/ContentView.swift
@@ -8,68 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var calculationLog = ""
+    @ObservedObject var calculate: Calculate = Calculate()
     @State private var inputNum = "0"
     
     var body: some View {
         VStack {
             Spacer()
             VStack(spacing: 150) { // Texts area
-                Text(calculationLog).font(.title).foregroundColor(.gray)
+                Text(calculate.calculationLog).font(.title).foregroundColor(.gray)
                 Text(inputNum).font(.largeTitle)
             }.frame(width: 360, alignment: .trailing)
             Spacer()
-            ButtonsView(calculationLog: $calculationLog, inputNum: $inputNum)
+            ButtonsView(calculate: calculate, inputNum: $inputNum)
         }
         .padding()
     }
 }
 
 struct ButtonsView: View {
-    @Binding var calculationLog: String
+    
+    @ObservedObject var calculate: Calculate
     @Binding var inputNum: String
     
     var body: some View {
         Group { // Buttons area
             HStack {
                 VStack {
-                    HStack{ // AC, BS, % Buttons
-                        Button(action: {print("AC or C")}, label: {Text("AC").modifier(buttonTextStyle())})
+                    HStack { // AC, BS, % Buttons
+                        inputNum == "0" ?
+                        Button(action: { allClear() }, label: {Text("AC").modifier(buttonTextStyle())}) :
+                        Button(action: { clear() }, label: {Text("C").modifier(buttonTextStyle())})
+                        
                         Button(action: {backSpace()}, label: {Image(systemName: "delete.backward.fill").modifier(buttonTextStyle())})
-                        Button(action: {print("%")}, label: {Text("%").modifier(buttonTextStyle())})
+                        Button(action: {calculate.operators("%")}, label: {Text("%").modifier(buttonTextStyle())})
                     }
                     .buttonStyle(.borderedProminent)
 
                     Group { // Numbers Buttons
-                        HStack{
-                            Button(action: {print("7")}, label: {Text("7").modifier(buttonTextStyle())})
-                            Button(action: {print("8")}, label: {Text("8").modifier(buttonTextStyle())})
-                            Button(action: {print("9")}, label: {Text("9").modifier(buttonTextStyle())})
+                        HStack {
+                            Button(action: {inputNumber("7")}, label: {Text("7").modifier(buttonTextStyle())})
+                            Button(action: {inputNumber("8")}, label: {Text("8").modifier(buttonTextStyle())})
+                            Button(action: {inputNumber("9")}, label: {Text("9").modifier(buttonTextStyle())})
                         }
-                        HStack{
-                            Button(action: {print("4")}, label: {Text("4").modifier(buttonTextStyle())})
-                            Button(action: {print("5")}, label: {Text("5").modifier(buttonTextStyle())})
-                            Button(action: {print("6")}, label: {Text("6").modifier(buttonTextStyle())})
+                        HStack {
+                            Button(action: {inputNumber("4")}, label: {Text("4").modifier(buttonTextStyle())})
+                            Button(action: {inputNumber("5")}, label: {Text("5").modifier(buttonTextStyle())})
+                            Button(action: {inputNumber("6")}, label: {Text("6").modifier(buttonTextStyle())})
                         }
-                        HStack{
-                            Button(action: {print("1")}, label: {Text("1").modifier(buttonTextStyle())})
-                            Button(action: {print("2")}, label: {Text("2").modifier(buttonTextStyle())})
-                            Button(action: {print("3")}, label: {Text("3").modifier(buttonTextStyle())})
+                        HStack {
+                            Button(action: {inputNumber("1")}, label: {Text("1").modifier(buttonTextStyle())})
+                            Button(action: {inputNumber("2")}, label: {Text("2").modifier(buttonTextStyle())})
+                            Button(action: {inputNumber("3")}, label: {Text("3").modifier(buttonTextStyle())})
                         }
-                        HStack{ // +/-, 0, . Buttons
-                            Button(action: {print("plus/minus")}, label: {Image(systemName: "plus.forwardslash.minus").modifier(buttonTextStyle())})
-                            Button(action: {print("0")}, label: {Text("0").modifier(buttonTextStyle())})
-                            Button(action: {print(".")}, label: {Text(".").modifier(buttonTextStyle())})
+                        HStack { // +/-, 0, . Buttons
+                            Button(action: {inputNumber("+/-")}, label: {Image(systemName: "plus.forwardslash.minus").modifier(buttonTextStyle())})
+                            Button(action: {inputNumber("0")}, label: {Text("0").modifier(buttonTextStyle())})
+                            Button(action: {inputNumber(".")}, label: {Text(".").modifier(buttonTextStyle())})
                         }
                     }
                     .buttonStyle(.bordered)
                 }
                 VStack { // Operator Buttons
-                    Button(action: {print("divide")}, label: {Image(systemName: "divide").modifier(buttonTextStyle())})
-                    Button(action: {print("multiply")}, label: {Image(systemName: "multiply").modifier(buttonTextStyle())})
-                    Button(action: {print("minus")}, label: {Image(systemName: "minus").modifier(buttonTextStyle())})
-                    Button(action: {print("plus")}, label: {Image(systemName: "plus").modifier(buttonTextStyle())})
-                    Button(action: {print("equal")}, label: {Image(systemName: "equal").modifier(buttonTextStyle())})
+                    Button(action: {
+                        calculate.inputValues.append(inputNum)
+                        calculate.operators("/")
+                    }, label: {Image(systemName: "divide").modifier(buttonTextStyle())})
+                    Button(action: {
+                        calculate.inputValues.append(inputNum)
+                        calculate.operators("*")
+                    }, label: {Image(systemName: "multiply").modifier(buttonTextStyle())})
+                    Button(action: {
+                        calculate.inputValues.append(inputNum)
+                        calculate.operators("-")
+                    }, label: {Image(systemName: "minus").modifier(buttonTextStyle())})
+                    Button(action: {
+                        calculate.inputValues.append(inputNum)
+                        calculate.operators("+")
+                    }, label: {Image(systemName: "plus").modifier(buttonTextStyle())})
+                    Button(action: {
+                        calculate.inputValues.append(inputNum)
+                        inputNum = calculate.equal()
+                    }, label: {Image(systemName: "equal").modifier(buttonTextStyle())})
                 }
                 .buttonStyle(.borderedProminent)
             }
@@ -86,11 +105,43 @@ struct ButtonsView: View {
         }
     }
     
-    func clear() {
-        inputNum = "0"
-    }
+    func clear() { inputNum = "0" }
     
     func allClear() { // Todo. 클래스 구현하고 구현하기
+        calculate.inputValues = []
+        calculate.calculationLog = ""
+    }
+    
+    func operatorButtons(_ input: String) {
+        
+    }
+    
+    func inputNumber(_ input: String) {
+        if inputNum == calculate.calculationResult {
+            allClear()
+            clear()
+        } else if inputNum == "-0" { inputNum.removeLast() }
+        else if ["+", "-", "*", "/"].contains(calculate.inputValues.last) { clear() }
+        
+        switch input {
+        case "0":
+            if inputNum != "0" { inputNum += "0" }
+        case "1"..."9":
+            if inputNum == "0" {
+                inputNum = input
+            } else {
+                inputNum += input
+            }
+        case ".":
+            if inputNum.isEmpty { inputNum += "0." }
+            else if inputNum == "0" { inputNum += "." }
+            else if !inputNum.contains(".") { inputNum += "." }
+        case "+/-":
+            if inputNum.contains("-") { inputNum.removeFirst() }
+            else { inputNum.insert("-", at: inputNum.startIndex) }
+        default:
+            break
+        }
         
     }
 }
@@ -105,6 +156,6 @@ struct buttonTextStyle: ViewModifier {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(calculate: Calculate())
     }
 }
