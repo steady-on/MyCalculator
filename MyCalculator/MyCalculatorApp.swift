7342005ab0ec3d0db5ca49e956205b628d08cbd0
whitebsm93 <whitebsm93@naver.com>
2022-10-29 23:06:39 +0900

    Initial commit

---
+++ MyCalculator/MyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalculatorApp.swift
+//  MyCalculator
+//
+//  Created by Ruhen White on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct MyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
